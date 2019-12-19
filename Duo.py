1f07dd7b17e0ea0589e451102f161ae81934f280
bobstoni11 <boston.horspool@gmail.com>
2019-12-19 14:45:50 -0700

    added tesseract

--- Duo.py
+++ Duo.py
@@ -2,8 +2,11 @@ import pyautogui as gui
 import webbrowser
 import time
 from matplotlib import pyplot as plt
-from pytesseract import image_to_string
+import pytesseract
 from googletrans import Translator
+
+pytesseract.pytesseract.tesseract_cmd = "tesseract/4.1.0/bin/tesseract"
+translator = Translator()
 
 def click(path, after=0):
     time.sleep(after)
@@ -13,18 +16,30 @@ def click(path, after=0):
         gui.moveTo(mid)
         gui.mouseDown()
         gui.mouseUp()
+        return True
     except:
         print(path + "button not found")
+        return False
         
 def getSentence():
-    pos=gui.locateOnScreen('screenshots/sound.png', grayscale=True, confidence=0.75)
-    region = (pos[0] + pos[2], pos[1], 500, pos[3])
-    screenshot = gui.screenshot(region=region)
-#     plt.imshow(screenshot)
-#     plt.show()
-    text = image_to_string(screenshot)
-    print(text)
+    try:
+        pos=gui.locateOnScreen('screenshots/sound.png', grayscale=True, confidence=0.5)
+        region = (pos[0] + pos[2], pos[1]-10, 800, pos[3] + 20)
+    except:
+        pos=gui.locateOnScreen('screenshots/write.png', grayscale=True, confidence=0.5)
+        region = (pos[0], pos[1] + pos[3] * 1.69, 800, pos[3] + 20)
+
     
+    try:
+        
+        screenshot = gui.screenshot(region=region)
+#         plt.imshow(screenshot)
+#         plt.show()
+        text = pytesseract.image_to_string(screenshot)
+    except TypeError:
+        print("no sentence")
+    
+    return text
         
 def openLesson(name, after=0):
     time.sleep(after)
@@ -33,8 +48,9 @@ def openLesson(name, after=0):
     gui.typewrite(name)
     time.sleep(.5)
     gui.press('esc')
-    click("screenshots/lessons/" + name + ".png", 2)
-    click('screenshots/start.png', 2)
+    click("screenshots/lessons/" + name + ".png", .5)
+    click('screenshots/start.png', .5)
+    click('screenshots/use-keyboard.png', 1)
 
 
 def login(user, passwd):
@@ -47,20 +63,32 @@ def login(user, passwd):
     click('screenshots/submit.png')
     
     
-def translate(sentance):
-    curLan = translator.detect(sentance).lang
+def translate(sentence):
+    curLan = translator.detect(sentence).lang
     if(curLan == 'en'):
-        translated = translator.translate(sentance, dest='de').text
+        translated = translator.translate(sentence, dest='de').text
     elif(curLan == 'de'):
-        translated = translator.translate(sentance, dest='en').text
+        translated = translator.translate(sentence, dest='en').text
+    return translated
     
+def inputSentence(sentence):
+    gui.typewrite(sentence + "\n", interval= 0.02)
+    gui.hotkey('enter')
+    
+def doLesson():
+    while True:
+        time.sleep(1)
+        sentence = getSentence()
+        if sentence == None:
+            continue
+        time.sleep(.5)
+        t = translate(sentence)
+        inputSentence(t)
 
 # login("21horspbosk@washk12.org","bobbob11")
 webbrowser.open('http://duolingo.com/', new=2)
-openLesson('plurals', 1)
-
-click('screenshots/use-keyboard.png', 1)
-getSentence()
+openLesson('adjectives', 1.3)
+doLesson()
 
 
 
